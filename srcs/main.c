19331901e4f732fafab08505ba36d8f0987158b9
Camille Baillat <camille.baillat@protonmail.com>
2017-12-17 21:00:12 +0100

    [dev] testing for solidity

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 14:06:57 by cbaillat          #+#    #+#             */
-/*   Updated: 2017/12/17 20:54:09 by cbaillat         ###   ########.fr       */
+/*   Updated: 2017/12/17 20:59:39 by cbaillat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	main(void)
 	// ft_printf("Mine: \n string: %s\n char: %c\n int: %d\n", arg1, arg2, arg3);
 	printf("\n\n=======================================\n");
 	printf("Testing width detection\n");
-	printf("Expression: %%-0.-#+*.*ld width=20 preci=10\n");
+	printf("Expression: %%-0.-#+*.10ld width=20\n");
 	printf("-------------------------------------------\n");
 	ft_printf("Mine2:\n%-0-#+*.10ld\n", 20);
 	printf("=======================================\n");
