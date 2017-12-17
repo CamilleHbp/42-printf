ac968fe012bcb658cb1657d9f51cc61a91cabd3a
Camille Baillat <camille.baillat@protonmail.com>
2017-12-17 21:44:30 +0100

    [printing] starting the work on the second phase

--- srcs/printing/arg_print.c
+++ srcs/printing/arg_print.c
@@ -6,7 +6,7 @@
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/15 22:28:32 by cbaillat          #+#    #+#             */
-/*   Updated: 2017/12/17 19:28:59 by cbaillat         ###   ########.fr       */
+/*   Updated: 2017/12/17 21:44:03 by cbaillat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,12 @@
 	// ;
 // }
 
-// int32_t		print_arg(t_format format, va_list *app)
-// {
-	// if (format.type == integer)
-		// print_integer(format, va_arg(*app, __int128_t));
-// }
+int32_t		print_arg(t_format format, va_list *app)
+{
+	if (format.specifier == "diu")
+		print_integer(format, *app));
+	else if (format.specifier == "f")
+		print_float(format, *app));
+	else if (format.specifier == "oOuUbUxX")
+		print_base(format, *app));
+}
