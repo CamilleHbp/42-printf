37dc84cc703c6bb62f2719e05ad4f75bd4fa71b9
Camille Baillat <camille.baillat@protonmail.com>
2017-12-29 22:24:44 +0100

    [clean] move the char function to the string file

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 14:06:57 by cbaillat          #+#    #+#             */
-/*   Updated: 2017/12/29 22:20:46 by cbaillat         ###   ########.fr       */
+/*   Updated: 2017/12/29 22:23:13 by cbaillat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,11 +37,4 @@ int	main(void)
 		wstring: |%-10S|\n \
 		wchar:   |%12C\n\n", \
 			arg1, arg2, arg3, arg4, arg5);
-	// ft_printf("Mine: \n string: %s\n char: %c\n int: %d\n", arg1, arg2, arg3);
-	/* printf("\n\n=======================================\n");
-	printf("Testing width detection\n");
-	printf("Expression: %%-0.-#+*.10ld width=20\n");
-	printf("-------------------------------------------\n");
-	ft_printf("Mine2:\n%-0-#+*.10ld\n", 20);
-	printf("=======================================\n") */;
 }
--- srcs/printing/strings.c
+++ srcs/printing/strings.c
@@ -6,12 +6,32 @@
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/26 18:34:20 by cbaillat          #+#    #+#             */
-/*   Updated: 2017/12/29 21:59:22 by cbaillat         ###   ########.fr       */
+/*   Updated: 2017/12/29 22:23:35 by cbaillat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "printing.h"
+
+size_t	print_char(t_format format, va_list *app, t_buffer *buffer)
+{
+	size_t	width;
+	int		printed_char;
+
+	width = (format.width > 0) ? format.width - 1: 0;
+	if (!(format.flags & RIGHT_PAD))
+		padd_value((format.flags & ZERO_PAD) ? "0" : " ", width, buffer);
+	if (format.flags & UNICODE)
+		buffer_wchar(va_arg(*app, wchar_t), buffer);
+	else
+	{
+		printed_char = va_arg(*app, int);
+		buffered_print(&printed_char, 1, buffer);
+	}
+	if (format.flags & RIGHT_PAD)
+		padd_value(" ", width, buffer);
+	return ((format.width > 1) ? format.width : 1);
+}
 
 size_t	print_string(t_format format, va_list *app, t_buffer *buffer)
 {
