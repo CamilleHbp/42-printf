879dcdf6355e6b845f5372c7903fa4b12ec81466
Camille Baillat <camille.baillat@protonmail.com>
2018-01-02 13:34:01 +0100

    [dev][floats] fixed the width flag (forgot to give the absolute value to get_len)

--- srcs/printing/nums_common_floats.c
+++ srcs/printing/nums_common_floats.c
@@ -6,11 +6,13 @@
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/31 11:23:59 by cbaillat          #+#    #+#             */
-/*   Updated: 2017/12/31 15:14:44 by cbaillat         ###   ########.fr       */
+/*   Updated: 2018/01/02 13:30:59 by cbaillat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printing.h"
+// DEBUG:
+#include <stdio.h>
 
 void	print_decimal(long double nb, uint8_t base, t_format format,
 			t_buffer *buffer)
@@ -39,16 +41,16 @@ void	print_float_number(long double nb, uint8_t base, t_format format,
 	if ((nb < 0) || (format.flags & SIGN) || (format.flags & SPACE))
 		format.width -= 1;
 	// If the format precision is not 0, we need to subtract 1 more for the '.'
-	format.width -= get_nb_len(integer, base);
+	format.width -= get_nb_len(ft_absl(integer), base);
 	format.width -= (format.precision == 0) ? 0 : format.precision + 1;
 	// If we need to right justify, and pad with spaces, we do before the prefix
 	if (!(format.flags & RIGHT_PAD) && !(format.flags & ZERO_PAD))
 		padd_value(" ", format.width, buffer);
-	// If we need to right justify, and pad with 0, we do that after the prefix
-	else if (!(format.flags & RIGHT_PAD) && (format.flags & ZERO_PAD))
-		padd_value("0", format.width, buffer);
 	if (nb < 0)
 		buffered_print("-", 1, buffer);
+	// If we need to right justify, and pad with 0, we do that after the prefix
+	if (!(format.flags & RIGHT_PAD) && (format.flags & ZERO_PAD))
+		padd_value("0", format.width, buffer);
 	// If precision is 0, we print only the rounded integer part
 	if (format.precision == 0)
 		print_itoa_base(ft_absl(ft_round(nb)), base, format, buffer);
